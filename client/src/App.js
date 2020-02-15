75756c9ae7854c3de4eb1256742594e56d051b65
Mike Raysor <michaelraysor1@gmail.com>
2020-02-15 00:36:05 -0500

    fixed bug in App.js section

--- client/src/App.js
+++ client/src/App.js
@@ -11,20 +11,22 @@ const App = () => {
 
   return (
     //Make it so that the card in `MovieList` is a link
-    <div className="movie-link">
-      <Link to= {`/movies/${key}`}>MovieList</Link>
-    </div>
     <div>
+      <div className="movie-link">
+        <Link to={`/movies/${key}`}>MovieList</Link>
+      </div>
       <SavedList list={savedList} />
        //Wrap your app with the router.
        //Inside your App file add two routes.
         - [ ] one route for `/` that loads the `MovieList` component.
-        - [ ] one route that will take an `id` parameter after`/movies/` 
+      - [ ] one route that will take an `id` parameter after`/movies/` 
       <Route path="/" component={MovieList} />
       <Route path="/movies/: id" component={Movies} />
     </div>
+
   );
 }
+  
 
 
 export default App;
